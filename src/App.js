6401600ba8336a1cae697e0bddfb397e03ae598f
Emre Kaçan <emrekacan@gmail.com>
2022-02-12 13:46:41 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import {motion} from 'framer-motion';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <motion.div
+                animate={{
+                    scale: [1, 1.2, 1],
+                    opacity: [1, 1, 1],
+                }}
+                transition={{
+                    duration: 3,
+                    ease: "easeIn",
+                    times: [0, 0.5, 1],
+                    repeat: Infinity,
+                    repeatDelay: 0
+                }}
+            >
+                <h1>
+                    Under<br/>Construction <br/><br/>Mona Software
+                </h1>
+            </motion.div>
+        </div>
+    );
 }
 
 export default App;
